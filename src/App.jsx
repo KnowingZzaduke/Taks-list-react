14f4fc1e890d98f9305b0b01f73971fc626e78cc
KnowingZzaduke <josex.developer@gmail.com>
2023-02-21 21:39:55 -0500

    Creando el formulario para agregar tareas

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import {Taks} from './Taks_list';
+export function HolaMundo(){
+    return <div>
+        <Taks/>
+    </div>
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { HolaMundo } from './App'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HolaMundo/>
+  </React.StrictMode>
+)
